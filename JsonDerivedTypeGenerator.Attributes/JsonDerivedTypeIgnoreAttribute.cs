fe7474f4c5fb4720f2ecec7c48c9a2916fdd2341
Jakub Tokarczyk <kubat260@gmail.com>
2026-05-17 22:15:40 +0200

    feat: added possibility to ignore specific implementation via [JsonDerivedTypeIgnore] attribute
    tests: added real integration tests with System.Text.Json.JsonSerializer

---
+++ JsonDerivedTypeGenerator.Attributes/JsonDerivedTypeIgnoreAttribute.cs
@@ -0,0 +1,4 @@
+namespace System.Text.Json.Serialization;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+public sealed class JsonDerivedTypeIgnoreAttribute : Attribute { }
